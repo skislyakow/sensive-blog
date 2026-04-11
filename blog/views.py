640c2c8fc367b6fc5f6847bcd8f59e10ad7bec29
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-04-11 15:14:43 +0300

    replace all sorted calls with order_by on main page

--- blog/views.py
+++ blog/views.py
@@ -43,9 +43,12 @@ def index(request):
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
 
-    tags = Tag.objects.all()
-    popular_tags = sorted(tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    #tags = Tag.objects.all()
+    #popular_tags = sorted(tags, key=get_related_posts_count)
+    #most_popular_tags = popular_tags[-5:]
+    most_popular_tags = Tag.objects.annotate(
+        tags_count=Count('posts')
+    ).order_by('-tags_count')[:5]
 
     context = {
         'most_popular_posts': [
@@ -84,11 +87,17 @@ def post_detail(request, slug):
         'tags': [serialize_tag(tag) for tag in related_tags],
     }
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    #all_tags = Tag.objects.all()
+    #popular_tags = sorted(all_tags, key=get_related_posts_count)
+    #most_popular_tags = popular_tags[-5:]
+    most_popular_tags = Tag.objects.annotate(
+        tags_count=Count('posts')
+    ).order_by('-tags_count')[:5]
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    #most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = Post.objects.annotate(
+        likes_count=Count('likes')
+    ).order_by('-likes_count')[:5]
 
     context = {
         'post': serialized_post,
@@ -103,11 +112,18 @@ def post_detail(request, slug):
 def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    #all_tags = Tag.objects.all()
+    #popular_tags = sorted(all_tags, key=get_related_posts_count)
+    #most_popular_tags = popular_tags[-5:]
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_tags = Tag.objects.annotate(
+        tags_count=Count('posts')
+    ).order_by('-tags_count')[:5]
+
+    #most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = Post.objects.annotate(
+        likes_count=Count('likes')
+    ).order_by('-likes_count')[:5]
 
     related_posts = tag.posts.all()[:20]
 
